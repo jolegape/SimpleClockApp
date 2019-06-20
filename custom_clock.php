cc5d27a152610a1648c4f6d9fa65f45514207242
Gavin <github@jolegape.com>
2019-06-21 09:02:33 +1000

    Improved smaller resolution compatibility
    Adjusted column sizing and CSS font sizes to better suit screens with resolutions as low as 1366x768.
    
    To Do: improve compatibility for tablet / phone size screens

--- custom_clock.php
+++ custom_clock.php
@@ -38,42 +38,42 @@
 	<div class="container-fluid h-100">
 		<div class="row h-100 align-items-center">
 			<div class="col-6" id="clockText">
-				<h3 class="display-3 text-center font-weight-bolder py-4"><?php echo $clockTitle; ?></h3>
+				<p class="text-center py-4 title"><?php echo $clockTitle; ?></p>
 				<div class="container">
-					<div class="row py-4">
-						<div class="col-6 h1 display-4">Start Time:</div>
-						<div class="col-6 h1 display-4 text-right"><?php echo $startTime; ?></div>
+					<div class="row py-2">
+						<div class="col-7 h1 display-4 time-heading">Start Time:</div>
+						<div class="col-5 h1 display-4 text-right time"><?php echo $startTime; ?></div>
 					</div>
 					
 					<?php if ("" != trim($_POST["45minWarning"])): ?>
-					<div class="row py-4">
-						<div class="col-6 h1 display-4">45min Warning:</div>
-						<div class="col-6 h1 display-4 text-right"><?php echo $warn45; ?></div>
+					<div class="row py-2">
+						<div class="col-7 h1 display-4 time-heading">45min Warning:</div>
+						<div class="col-5 h1 display-4 text-right time"><?php echo $warn45; ?></div>
 					</div>
 					<?php endif; ?>
 
 					<?php if ("" != trim($_POST["45minWarning"])): ?>
-					<div class="row py-4">
-						<div class="col-6 h1 display-4">30min Warning:</div>
-						<div class="col-6 h1 display-4 text-right"><?php echo $warn30; ?></div>
+					<div class="row py-2">
+						<div class="col-7 h1 display-4 time-heading">30min Warning:</div>
+						<div class="col-5 h1 display-4 text-right time"><?php echo $warn30; ?></div>
 					</div>
 					<?php endif; ?>
 
 					<?php if ("" != trim($_POST["45minWarning"])): ?>
-					<div class="row py-4">
-						<div class="col-6 h1 display-4">10min Warning:</div>
-						<div class="col-6 h1 display-4 text-right"><?php echo $warn10; ?></div>
+					<div class="row py-2">
+						<div class="col-7 h1 display-4 time-heading">10min Warning:</div>
+						<div class="col-5 h1 display-4 text-right time"><?php echo $warn10; ?></div>
 					</div>
 					<?php endif; ?>
 
-					<div class="row py-4">
-						<div class="col-6 h1 display-4">Finish Time:</div>
-						<div class="col-6 h1 display-4 text-right"><?php echo $finishTime; ?></div>
+					<div class="row py-2">
+						<div class="col-7 h1 display-4 time-heading">Finish Time:</div>
+						<div class="col-5 h1 display-4 text-right time"><?php echo $finishTime; ?></div>
 					</div>
 				</div>
 			</div>
 			<div class="col-6">
-				<h3 class="display-3 text-center font-weight-bold" id="clockDisplay"></h3>
+				<h4 class="display-4 text-center font-weight-bolder" id="clockDisplay"></h4>
 			</div>
 		</div>
 	</div>
